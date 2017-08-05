155061af3bbf77f3e8eac8ed7176e7167ef39488
flickerstop <maclean.joey@gmail.com>
2017-08-05 07:51:15 -0700

    To the beach, to the beach, to the beach beach beach!

--- src/objects/Globals.java
+++ src/objects/Globals.java
@@ -13,8 +13,8 @@ public class Globals {
 	/////////////////////////////
 	// SAVE EDIT MODE
 	// Is it safe to edit the files
-	public static boolean isSafeEdit = true;
-	public static String versionNumber = "0.6.1";
+	public static boolean isSafeEdit = false;
+	public static String versionNumber = "0.6.2";
 	
 	public static String path = System.getenv("APPDATA")+"\\SlayerTracker";
 	public static String savePath = System.getenv("APPDATA")+"\\SlayerTracker\\player.sav";
